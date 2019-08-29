191fcbb6d685c9f9552382f2974601a81d81290e
Tony Major <tonymajordev@outlook.com>
2019-08-28 22:28:58 -0400

    Added publish folder

--- TeslaCamViewer/TeslaCamViewer/Properties/AssemblyInfo.cs
+++ TeslaCamViewer/TeslaCamViewer/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TeslaCam Viewer")]
+[assembly: AssemblyTitle("TeslaCamViewer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TeslaCam Viewer")]
-[assembly: AssemblyCopyright("Copyright ©  2019 mattw01")]
+[assembly: AssemblyProduct("TeslaCamViewer")]
+[assembly: AssemblyCopyright("Copyright © 2019 TonyMajorDev")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.4.2.0")]
-[assembly: AssemblyFileVersion("0.4.2.0")]
+[assembly: AssemblyVersion("0.5.*")]
+[assembly: AssemblyFileVersion("0.5.0.0")]
